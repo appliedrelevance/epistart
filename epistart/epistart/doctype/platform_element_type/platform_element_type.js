685a70dab2b9b046126e7dad8fec40dfe922bce8
George Everitt <geveritt@oproma.com>
2024-04-21 20:46:21 +0000

    Added Product Requirements Document doctype

---
+++ epistart/epistart/doctype/platform_element_type/platform_element_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Applied Relevance and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Platform Element Type", {
+// 	refresh(frm) {
+
+// 	},
+// });

3c46aa7fa671fd3be7c8f23f91fcb7baa65a528d
Geordie Everitt <geveritt@appliedrelevance.com>
2024-06-27 20:52:10 +0000

    Updated PRD and Activities

--- epistart/epistart/doctype/local_activity/local_activity.js
+++ epistart/epistart/doctype/local_activity/local_activity.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Applied Relevance and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Product Requirements Document", {
+// frappe.ui.form.on("Local Activity", {
 // 	refresh(frm) {
 
 // 	},

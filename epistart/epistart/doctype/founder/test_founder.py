90853fe4aa93300e38ebefb2ea42dd48d22ce361
Geordie Everitt <geveritt@appliedrelevance.com>
2024-07-28 20:30:17 +0000

    WIP - Merging EpiFund into EpiStart

---
+++ epistart/epistart/doctype/founder/test_founder.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Applied Relevance and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFounder(FrappeTestCase):
+	pass
